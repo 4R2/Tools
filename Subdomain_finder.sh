677e59c1d05d8d6688564ba7ea29dc1ff10b4cc3
4R2 <43121411+4R2@users.noreply.github.com>
2025-11-22 19:49:36 +0200

    Create Subdomain_finder.sh

---
+++ Subdomain_finder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then echo "How to use : ./sub_finder.sh <domain>"
+echo "Ex: ./sub_finder.sh www.facebook.com"
+
+else
+wget $1 2> /dev/null && less index.html | grep "href=" | cut -d ":" -f 2 | cut -d "/" -f 3 | grep "mega" | cut -d '"' -f 1 | grep -v "$1" | uniq > sub.txt
+
+
+for sub in $(cat sub.txt)
+do
+if [[ $(ping -c 1 $sub  2> /dev/null) ]]
+then
+echo "$sub +++++ Pong"
+echo $sub >> valid_sub.txt
+else
+echo "$sub ----- Error"
+fi
+done
+
+for ip in $(cat valid_sub.txt)
+do
+host $ip
+host $ip |cut -d " " -f 4 | uniq >> ips.txt
+done
+echo " DONE ......"
+fi
